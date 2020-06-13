2121193082af25a21252d56458769038a7c8a960
dib1987 <d.mondal87@gmail.com>
2020-06-14 00:36:34 +0530

    changes made by oscar

--- src/main/java/com/orangehrmlive/setup/DriverSetup.java
+++ src/main/java/com/orangehrmlive/setup/DriverSetup.java
@@ -16,7 +16,10 @@ public class DriverSetup {
 	public WebDriver driver;
 	public static Properties prop;
 	
-	private static String WINDOWS_DRIVER = "driver/chromedriver.exe";
+	//private static String WINDOWS_DRIVER = "driver/chromedriver.exe";
+	
+	//Added by Dibyendu//
+	public static String path = System.getProperty("user.dir");
 	
 	public DriverSetup() {			
 		prop = new Properties();
@@ -39,8 +42,11 @@ public class DriverSetup {
 		switch (browserName.toLowerCase()){
 	        case "chrome":
 	            ChromeOptions options = new ChromeOptions();
-	            String sDriver =getClass().getClassLoader().getResource(WINDOWS_DRIVER).getFile();
-	            System.setProperty("webdriver.chrome.driver", urlDecode(sDriver) );
+	            
+	            //String sDriver =getClass().getClassLoader().getResource(WINDOWS_DRIVER).getFile();
+	            //System.setProperty("webdriver.chrome.driver", urlDecode(sDriver) );
+	            
+	            System.setProperty("webdriver.chrome.driver", path+"\\src\\main\\java\\resource\\chromedriver.exe" );
 	            driver = new ChromeDriver(options);
 	            break;	        
 	        default:
